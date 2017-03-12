b438981457ae973d5fb440a75768511d1c657f94
Untuned <Untuned@users.noreply.github.com>
2017-03-12 13:17:33 -0400

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3729,7 +3729,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat('/me Untuned is the creator of SakiBot.);
+                        API.sendChat('/me Untuned is the creator of SakiBot.');
                     }
                 }
             },
